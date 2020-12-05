f7aa6de8706b7f5c0b3329a1f5b5738b2f50d000
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-12-06 00:32:50 +1100

    Day 3 Part One completed

--- 2020/day3/Part1.ts
+++ 2020/day3/Part1.ts
@@ -3,6 +3,20 @@ import * as fs from "fs";
 const map = fs
   .readFileSync("./2020/day3/input.txt", "utf-8")
   .split(/\s+|\n/g)
-  .map((slope) => slope + slope + slope + slope);
+  .map((slope) => slope.repeat(32));
 
-console.log(map);
+function getTress(): string[] {
+  const path: string[] = [];
+  let last: number = 0;
+  for (let i = 0; i < map.length; i++) {
+    last += 3;
+    if (map[i + 1]) path.push(map[i + 1][last]);
+  }
+  return path.filter((str) => str === "#");
+}
+
+const solution = getTress().length;
+
+console.log("Solution:", solution);
+
+// 28m 26s : Completion
